5d659f71701b37bda390745a6d792dde4e67440e
pnly <pnlybubbles@gmail.com>
2022-10-19 13:34:22 +0900

    test using . to resolve assets

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@ const isProd = process.env.NODE_ENV === "production";
 
 /** @type {import('next').NextConfig} */
 const config = {
-  assetPrefix: isProd ? process.env.ASSET_PREFIX : undefined,
+  assetPrefix: isProd ? "." : undefined,
 };
 
 module.exports = config;
